19145adc4e9bacc4ae0d9c9a617581841a670065
ChloeArd <chloe.ardoise@gmail.com>
2020-12-07 21:58:24 +0100

    Calculatrice

--- app.js
+++ app.js
@@ -41,9 +41,36 @@ function nombre (x) {
 
 let zero = document.getElementsByClassName("zero");
 zero.addEventListener("click", nombre("0"));
+
 let un = document.getElementsByClassName("un");
 un.addEventListener("click", nombre("1"));
 
+let deux = document.getElementsByClassName("deux");
+deux.addEventListener("click", nombre("2"));
+
+let trois = document.getElementsByClassName("un");
+trois.addEventListener("click", nombre("3"));
+
+let quatre = document.getElementsByClassName("quatre");
+quatre.addEventListener("click", nombre("4"));
+
+let cinq = document.getElementsByClassName("cinq");
+cinq.addEventListener("click", nombre("5"));
+
+let six = document.getElementsByClassName("six");
+six.addEventListener("click", nombre("6"));
+
+let sept = document.getElementsByClassName("sept");
+sept.addEventListener("click", nombre("7"))
+
+let huit = document.getElementsByClassName("huit");
+huit.addEventListener("click", nombre("8"));
+
+let neuf = document.getElementsByClassName("neuf");
+neuf.addEventListener("click", nombre("9"));
+
+let virgule = document.getElementsByClassName("virgule");
+virgule.addEventListener("click", nombre("."));
 
 
 
